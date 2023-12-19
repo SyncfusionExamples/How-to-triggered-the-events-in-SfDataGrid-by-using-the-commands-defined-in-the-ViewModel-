49c94e25a0ef5e7eb00e480d027e04825e90a88c
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2023-12-19 17:09:41 +0530

    Add files via upload

---
+++ SfDataGridSample/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls.Compatibility;
+using Syncfusion.Maui.Data;
+using Syncfusion.Maui.DataGrid;
+using Syncfusion.Maui.DataGrid.Helper;
+using System.Diagnostics;
+
+namespace SfDataGridSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
